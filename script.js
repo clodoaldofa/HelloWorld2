34c7d3a742e7c0dc770c621c9e60c717f1a931da
Bueno de Assis <clodoaldofa@hotmail.com>
2024-01-23 02:25:14 -0300

    Corrige os arquivos css e js...

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-#hello-world {
-  color: red;
-}
+const colors = ['red', 'green', 'yellow'];
+let colorIndex = 0;
+const h1 = document.querySelector('#hello-world');
+h1.addEventListener('click', () => {
+  colorIndex = (colorIndex + 1) % colors.length;
+  h1.style.color = colors[colorIndex];
+});
